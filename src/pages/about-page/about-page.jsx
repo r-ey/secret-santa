181b6641e326da94c4f3ddd5b6c2debce113451c
creamlaflare <antonkazachenko2002@gmail.com>
2023-11-19 13:47:49 -0800

    Update about-page content and make design changes to various elements

--- src/pages/about-page/about-page.jsx
+++ src/pages/about-page/about-page.jsx
@@ -7,7 +7,13 @@ function AboutPage() {
     <div className={styles.app}>
       <AppHeader />
       <main className={styles.mainSize}>
-        <div className={styles.aboutText}>tmp</div>
+        <div className={styles.aboutText}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et quod, sit. Atque eum quia
+          temporibus. Autem dolore id iure. Accusantium consequatur cupiditate deleniti
+          distinctio eum impedit porro provident, quisquam recusandae reprehenderit tempora tempore
+          voluptas, voluptate! Aliquid corporis cumque, deleniti doloremque esse et nisi officiis
+          possimus quasi soluta unde veniam, voluptas?
+        </div>
       </main>
     </div>
   );
