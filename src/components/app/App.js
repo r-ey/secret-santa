994482ee4496bf8958765ed62532483f5adf551c
creamlaflare <antonkazachenko2002@gmail.com>
2023-11-19 09:24:14 -0800

    Implement routing and code organization

---
+++ src/components/app/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Routes, Route,
+} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      {/* <Route path="/about" element={<AboutPage />} /> */}
+      <Route path="/login" element={<LoginPage />} />
+      {/* <Route path="/profile" element={<ProfilePage />} /> */}
+      {/* <Route path="/create-account" element={<CreateAccountPage />} /> */}
+      {/* <Route path="*" element={<NotFoundPage />} /> */}
+    </Routes>
+  );
+}
+
+export default App;
