cb846fbc717406fa1c25e8fab7c8ddbcd8df3c29
Jake Yeo <jyeo03@students.cs.ubc.ca>
2023-11-19 09:41:10 -0800

    Added Event object

--- backend/model/Event.java
+++ backend/model/Event.java
@@ -1,5 +1,24 @@
 package secret-santa.backend;
+import java.util.ArrayList;
 
 public class Event {
+
+    ArrayList<String> participants;
     
+    Event() {
+
+    }
+
+    public void addParticipant(String name) {
+        if (!participants.contains(name)) {
+            participants.add(name);
+        }
+    }
+
+    public void removeParticipant(String name) {
+        participants.remove(name);
+    }
+
+
+
 }
