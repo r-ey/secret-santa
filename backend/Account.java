7a7e0456183d11331d3363255a1f605ad687bfcc
Jake Yeo <jyeo03@students.cs.ubc.ca>
2023-11-19 09:21:53 -0800

    Added basic account functionalities

---
+++ backend/Account.java
@@ -0,0 +1,25 @@
+public class Account {
+    String username;
+    String password;
+
+    Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+}
