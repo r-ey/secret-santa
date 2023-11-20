c582b7b40ebd124c7519a3361cfc41ed4857a163
creamlaflare <antonkazachenko2002@gmail.com>
2023-11-19 16:45:33 -0800

    Add group creation functionality and modal UI

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppHeader from '../../components/app-header/app-header';
 import styles from './home-page.module.css';
 import Modal from '../../components/modal/modal';
@@ -7,6 +7,40 @@ import data from '../../utils/data.js';
 
 function HomePage() {
   const { isModalOpen, openModal, closeModal } = useModal();
+  const [groupDetails, setGroupDetails] = useState({
+    name: '',
+    budget: '',
+    people: [{ name: '', preferences: '' }],
+  });
+
+  const deletePerson = (index) => {
+    const newPeople = groupDetails.people.filter((_, idx) => idx !== index);
+    setGroupDetails({ ...groupDetails, people: newPeople });
+  };
+
+  const handleInputChange = (index, event) => {
+    const newPeople = groupDetails.people.map((person, idx) => {
+      if (idx === index) {
+        return { ...person, [event.target.name]: event.target.value };
+      }
+      return person;
+    });
+
+    setGroupDetails({ ...groupDetails, people: newPeople });
+  };
+
+  const addPerson = () => {
+    setGroupDetails({
+      ...groupDetails,
+      people: [...groupDetails.people, { name: '', preferences: '' }],
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic
+  };
+
   const sara = data[0];
   return (
     <div className={styles.app}>
@@ -33,11 +67,58 @@ function HomePage() {
         </div>
       </main>
       {
-        isModalOpen ? (
+        isModalOpen && (
           <Modal onClose={closeModal}>
-            <div>tmp</div>
+            <h3 className={styles.modalHeader}>Create a New Group</h3>
+            <div className={styles.modalTextBox}>
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <div>
+                      <input
+                        type="text"
+                        placeholder="Group Name"
+                        className={styles.requirementText}
+                        value={groupDetails.name}
+                        onChange={(e) => setGroupDetails({ ...groupDetails, name: e.target.value })}
+                      />
+                      <input
+                        type="number"
+                        placeholder="Budget Per Person"
+                        className={styles.requirementText}
+                        value={groupDetails.budget}
+                        /* eslint-disable-next-line max-len */
+                        onChange={(e) => setGroupDetails({ ...groupDetails, budget: e.target.value })}
+                      />
+                    </div>
+                    {groupDetails.people.map((person, index) => (
+                      // eslint-disable-next-line react/no-array-index-key
+                      <div key={index}>
+                        <input
+                          type="text"
+                          name="name"
+                          placeholder="Name"
+                          value={person.name}
+                          onChange={(e) => handleInputChange(index, e)}
+                        />
+                        <input
+                          type="text"
+                          name="preferences"
+                          placeholder="Preferences"
+                          value={person.preferences}
+                          onChange={(e) => handleInputChange(index, e)}
+                        />
+                        <button type="button" onClick={() => deletePerson(index)}>Delete</button>
+                      </div>
+                    ))}
+                    <button type="button" onClick={addPerson}>+ Add Person</button>
+                    <button type="submit">Submit</button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </Modal>
-        ) : null
+        )
       }
     </div>
   );
